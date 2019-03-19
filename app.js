3b9f0b0f668bfd4dc265ae943ad7e06e257fdea9
Leonidas1337 <48692849+Leonidas1337@users.noreply.github.com>
2019-03-19 22:12:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 /*
 
-    KUBBO CMS - main
+* * * * * * * * * * * * * * 
+*                         *
+*   #     #   # # # #     *
+*   #     #         #     *
+*   # # # #       #       *
+*   #     #     #         *
+*   #     #    # # # #    *
+*                    CMS  *                 
+* * * * * * * * * * * * * *
+Powered by hzCMS 
+Website: https://habbo.zone
 
+Created by Habbo.Zone Development Team
+HolyFuture (Coding) 
+and Leonidas (Webdesign)
+Discord: https://discord.gg/3HZfVkm
 */
 
 const express       = require("express");
